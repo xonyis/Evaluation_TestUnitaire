bb928b068f0532372e28d8b9130c247919487909
Julian <julia@MacBook-Pro-de-Julian.local>
2023-12-21 15:58:51 +0100

    ajout test htmlformathelper

---
+++ EvaluationSampleCode.UnitsTest/HtmlFormatHelper.UnitsTest.cs
@@ -0,0 +1,47 @@
+﻿namespace EvaluationSampleCode.UnitsTest;
+
+[TestClass]
+public class HtmlFormatHelpersTests
+{
+    [TestMethod]
+    public void GetBoldFormat_ReturnsBoldTxt()
+    {
+        // Arrange
+        var helper = new HtmlFormatHelper();
+        var content = "Ceci est un test";
+
+        // Act
+        var result = helper.GetBoldFormat(content);
+
+        // Assert
+        Assert.AreEqual("<b>Ceci est un test</b>", result);
+    }
+
+    [TestMethod]
+    public void GetItalicFormat_ReturnsItalicTxt()
+    {
+        // Arrange
+        var helper = new HtmlFormatHelper();
+        var content = "Hello world";
+
+        // Act
+        var result = helper.GetItalicFormat(content);
+
+        // Assert
+        Assert.AreEqual("<i>Hello world</i>", result);
+    }
+
+    [TestMethod]
+    public void GetFormattedListElements_ReturnsFormattedList()
+    {
+        // Arrange
+        var helper = new HtmlFormatHelper();
+        var contents = new List<string> {"1","2","3","4"};
+
+        // Act
+        var result = helper.GetFormattedListElements(contents);
+
+        // Assert
+        Assert.AreEqual("<ul><li>1</li><li>2</li><li>3</li><li>4</li></ul>", result);
+    }
+}
